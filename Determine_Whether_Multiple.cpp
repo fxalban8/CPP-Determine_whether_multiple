dd472c0fe6e5550c2c50e5d3daff0900d656eb53
fxalban08 <francisco.alban@epn.edu.ec>
2020-06-02 17:07:59 -0500

    Asking the user to enter two numbers

--- Determine_Whether_Multiple.cpp
+++ Determine_Whether_Multiple.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+
+int main() {
+	int n,m;
+	
+	cout<<"Enter one number: "<<flush;
+	cin>>n;
+
+	cout<<"Enter another number: "<<flush;
+	cin>>m;
+
+	
+
+	return 0;
+}
